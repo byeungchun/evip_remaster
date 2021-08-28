c14af47b8ecf0edfa364e8e24999ce42e7d006b5
Byeungchun Kwon <byeungchun.kwon@gmail.com>
2021-08-28 02:13:11 +0200

    add calculate_wt_mt_comparison logic

--- evip2/utility/evip.py
+++ evip2/utility/evip.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import pandas as pd
-from scipy.stats import wilcoxon
+from scipy.stats import wilcoxon, kruskal
 
 def calculate_corr_sample(df:pd.DataFrame)->pd.DataFrame:
     """ calculate correlation of RNA samples 
@@ -41,10 +41,7 @@ def calculate_wildtype_distribution(df: pd.DataFrame, rep_null_dist:list, wt_nam
     """ eVIP buildWT_dict """
 
     df_wt = df.loc[[x.startswith(wt_name) for x in df.index],[x.startswith(wt_name) for x in df.columns]]
-    np.fill_diagonal(df_wt.values, np.nan)
-
-    rep_rankpts = df_wt.apply(lambda x: np.percentile(x.dropna().values, 50), axis=1).values
-    wt_rep_rankpt = np.percentile(rep_rankpts, 50)
+    wt_rep_rankpt, rep_rankpts = _calculate_self_connectivity(df_wt)
 
     wt_rep_pval = wilcoxon(rep_rankpts, [rep_null_dist[0] for _ in rep_rankpts]).pvalue
 
@@ -53,3 +50,53 @@ def calculate_wildtype_distribution(df: pd.DataFrame, rep_null_dist:list, wt_nam
     ret3 = [wt_name]
 
     return ret1, ret2, ret3
+
+def calculate_wt_mt_comparison(df: pd.DataFrame, rep_null_dist:list, conn_null_dist:list, wt_dict: dict, sig_id:dict)->dict:
+    """ eVIP Build comparison part """
+
+    mut_rep_pvals = list()
+    mut_wt_conn_pvals = list()
+    mut_wt_rep_pvals = list()
+    wt_mut_rep_vs_wt_mut_conn_pvals = list()
+
+    wt_name = [k for k,v in sig_id.items() if v['type']=='WT'][0]
+
+    for cell_key, cell_val in sig_id.items():
+        if cell_val['type'] != 'MT':
+            continue
+        
+        #STEP1: calculate self connectivity and p-value
+        df_mt = df.loc[[x.startswith(cell_key) for x in df.index],[x.startswith(cell_key) for x in df.columns]]
+        mut_rankpt, mut_rankpt_dist = _calculate_self_connectivity(df_mt)
+        self_pval = wilcoxon(mut_rankpt_dist, [rep_null_dist[0] for _ in mut_rankpt_dist]).pvalue
+        mut_rep_pvals.append(self_pval)
+
+        #STEP2: calculate connectivity and p-value
+        df_mt_wt = df.loc[[x.startswith(wt_name) for x in df.index],[x.startswith(cell_key) for x in df.columns]]
+        mut_wt_conn_dist = list(df_mt_wt.apply(lambda x: np.percentile(x.values, 50)).values)
+        mut_wt_conn_dist.extend(list(df_mt_wt.apply(lambda x: np.percentile(x.values, 50), axis=1).values))
+        mut_wt_conn_rankpt = np.percentile(mut_wt_conn_dist, 50)
+        conn_pval = wilcoxon(mut_wt_conn_dist, [conn_null_dist[0] for _ in mut_wt_conn_dist]).pvalue
+        mut_wt_conn_pvals.append(conn_pval)
+
+        #STEP3: connectivity between mutant self connectivity and wildtype dist.
+        mut_wt_rep_pval = wilcoxon(mut_rankpt_dist, wt_dict[wt_name]['wt_rep_dist']).pvalue
+        mut_wt_rep_pvals.append(mut_wt_rep_pval)
+
+        #STEP4: Kruskal test
+        wt_mut_rep_vs_wt_mut_conn_pval = kruskal(wt_dict[wt_name]['wt_rep_dist'], mut_rankpt_dist, mut_wt_conn_dist).pvalue
+        wt_mut_rep_vs_wt_mut_conn_pvals.append(wt_mut_rep_vs_wt_mut_conn_pval)
+
+        ##TODO: check return values, implement corrected pvalue calculation
+
+
+    return mut_rep_pvals, mut_wt_conn_pvals, mut_wt_rep_pvals, wt_mut_rep_vs_wt_mut_conn_pvals
+
+def _calculate_self_connectivity(df:pd.DataFrame)->list:
+    """ get median values """
+    
+    np.fill_diagonal(df.values, np.nan)
+    rep_rankpts = df.apply(lambda x: np.percentile(x.dropna().values, 50), axis=1).values
+    wt_rep_rankpt = np.percentile(rep_rankpts, 50)
+
+    return wt_rep_rankpt, rep_rankpts
